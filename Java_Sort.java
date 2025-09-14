298cc977e82b7e86b8fc7863d7292dd051abba65
EntasaDev <vasy.saimon@gmail.com>
2025-09-14 23:26:12 +0300

    feature(added bubble_sort)

--- Java_Sort.java
+++ Java_Sort.java
@@ -1,5 +1,15 @@
 public class Java_Sort {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Good Night!")
+    }
+
+    public static void bubble_sort(*array){
+        for(int i = 0; i < array.size(); i = i+1){
+            for(int j = 0; j < array.size(); j = j+1)
+                if(array.get_elem(j)>array.get_elem(j+1)){
+                    Swap(array.get_elem(j),array.get_elem(j+1));
+                }
+        }
     }
 }
